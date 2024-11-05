d52400bb51fb094e9e52dd1010ef9e53cf62a065
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-11-05 21:40:10 +0200

    add components.txt

--- make.sh
+++ make.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-##############################################################################################################
 
 function priv_clippit
 (
@@ -9,6 +8,42 @@ Options:
     build   Build program
 EOF
 )
+
+function priv_lazbuild
+(
+    if ! (which lazbuild); then
+        source '/etc/os-release'
+        case ${ID:?} in
+            debian | ubuntu)
+                sudo apt-get update
+                sudo apt-get install -y lazarus
+                ;;
+        esac
+    fi
+    if [[ -f 'use/components.txt' ]]; then
+        git submodule update --init --recursive
+        git submodule update --recursive --remote
+        while read -r; do
+            if ! (lazbuild --verbose-pkgsearch "${REPLY}"); then
+                if ! (lazbuild --add-package "${REPLY}"); then
+                    if ! [[ -f "use/${REPLY}" ]]; then
+                        declare -A VAR=(
+                            [url]="https://packages.lazarus-ide.org/${REPLY}.zip"
+                            [out]=$(mktemp)
+                        )
+                        wget --output-document "${VAR[out]}" "${VAR[url]}" >/dev/null
+                        unzip -o "${VAR[out]}" -d "use/${REPLY}"
+                        rm --verbose "${VAR[out]}"
+                    fi
+                fi
+            fi
+        done < 'use/components.txt'
+        find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} +
+    fi
+    find 'src' -type 'f' -name '*.lpi' \
+        -exec lazbuild --no-write-project --recursive --no-write-project --build-mode=release {} + 1>&2
+)
+
 
 function priv_dpkg
 (
@@ -29,30 +64,13 @@ function priv_main
     set -euo pipefail
     if ((${#})); then
         case ${1} in
-            build)
-                if ! (which 'lazbuild'); then
-                    source '/etc/os-release'
-                    case ${ID:?} in
-                        debian | ubuntu)
-                            sudo apt-get update
-                            sudo apt-get install -y lazarus
-                            ;;
-                    esac
-                fi
-                if [[ -d 'use' ]]; then
-                    git submodule update --recursive --init
-                    git submodule update --recursive --remote
-                    find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} +
-                fi
-                find 'src' -type 'f' -name '*.lpi' -exec \
-                    lazbuild --no-write-project --recursive --no-write-project --build-mode=release {} + 1>&2
-                ;;
+            build) priv_lazbuild ;;
             dpkg) priv_dpkg ;;
+            *) priv_clippit ;;
         esac
     else
         priv_clippit
     fi
 )
 
-##############################################################################################################
 priv_main "${@}" >/dev/null
